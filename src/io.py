26234e790254dc0194a2766b57becde4fd6af734
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2022-04-25 11:11:11 -0400

    save and load are now all in one object, along with a multi-line input parser.

--- src/io.py
+++ src/io.py
@@ -4,21 +4,34 @@ import json
 class _IOManager:
     mats_path: str = 'data/MATS.json'
 
-    def load_mats(self) -> dict:
+    def load(self) -> dict:
         with open(self.mats_path) as file:
             return json.load(file)
 
-    def save_mats(self, data: dict) -> None:
-        current_mats = self.load_mats()
-        current_mats.update(data)
+    def save(self, data: dict) -> None:
         with open(self.mats_path, 'w') as file:
-            json.dump(current_mats, file)
+            json.dump(data, file)
+
+    def multi_input(self, prompt='') -> str:
+        input_list = [None]
+        while input_list[-1] != '':
+            print(prompt)
+            input_list.append(input())
+        return "\n".join(input_list[1:-1])
+
+
 
 
 sentinel = None
 
 
 def IOManager() -> _IOManager:
+    """
+    Singleton object containing i/o functions for this program.
+
+    :return: _IOManager object
+    """
+
     global sentinel
     if sentinel is None:
         sentinel = _IOManager()
--- test/test_io.py
+++ test/test_io.py
@@ -6,19 +6,40 @@ from unittest.mock import patch, PropertyMock
 from src.io import IOManager
 
 
+class TestLoad(unittest.TestCase):
+
+    def test_loader_target_file(self):
+        path = 'data/MATS.json'
+        self.assertEqual(path, IOManager().mats_path)
+
+    def test_loader_opens_json(self):
+        data = {'key', 'value'}
+        with patch('json.load', return_value=data):
+            self.assertEqual(data, IOManager().load())
+
+
 class TestSave(unittest.TestCase):
     def test_save_mats(self):
         data = {'key': 'value'}
         with file_write_cm() as cm:
             with patch('src.io._IOManager.mats_path', new_callable=PropertyMock) as mock:
                 mock.return_value = cm.path
-                IOManager().save_mats(data)
+                IOManager().save(data)
                 with open(cm.path) as file:
                     self.assertEqual(data, json.load(file))
 
 
+class TestInput(unittest.TestCase):
+    def test_get_input(self):
+        input_list = ["one", "two", "three", ""]
+        expected = "\n".join(input_list[:-1])
+        with patch("builtins.input", side_effect=input_list):
+            data = IOManager().multi_input()
+            self.assertEqual(expected, data)
+
+
 class file_write_cm:
-    path = 'test\\test_io\\test_write.json'
+    path = 'test\\test_write.json'
 
     def __enter__(self):
         f = open(self.path, 'w')
